1754ff2d02425a3887963e71002ae073918d305d
urbantumbleweeed <saunders.stephen@gmail.com>
2014-04-25 11:10:21 -0400

    Create action and params hash are completed

--- project_2_test/app/controllers/memes_controller.rb
+++ project_2_test/app/controllers/memes_controller.rb
@@ -1,5 +1,19 @@
 class MemesController < ApplicationController
 
+	def create
+		@meme = Meme.create(memes_params)
+
+		respond_to do |format|
+			format.html { redirect_to @meme}
+			format.js
+			format.json { render @meme.to_json}
+		end
+	end
+
+	private
+	def memes_params
+		params.require(:memes).permit(:name, :hipster_lvl, :outdated_ness, :glasses_worn)
+		
+	end
 
 end
-
